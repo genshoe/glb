6a634f0b67b6cd334b3647a0cddb06269aa81b69
genshoe <genshoeaddon@gmail.com>
2026-07-30 14:14:08 +0700

    Add files via upload

---
+++ assets/normal.vertex-B3IyANyp.js
@@ -0,0 +1,68 @@
+import{t as e}from"./shaderStore-D-XQlhUT.js";import{$ as t,At as n,Ct as r,Et as i,Q as a,St as o,X as s,Z as c,et as l,i as u,kt as d,ot as f,st as p,tt as m,xt as h}from"./index-GDLd02NM.js";var g=`normalVertexShader`,_=`attribute position: vec3f;
+#ifdef NORMAL
+attribute normal: vec3f;
+#endif
+#ifdef UV1
+attribute uv: vec2f;
+#endif
+#ifdef UV2
+attribute uv2: vec2f;
+#endif
+#ifdef VERTEXCOLOR
+attribute color: vec4f;
+#endif
+#include<bonesDeclaration>
+#include<bakedVertexAnimationDeclaration>
+#include<instancesDeclaration>
+uniform view: mat4x4f;uniform viewProjection: mat4x4f;
+#ifdef DIFFUSE
+varying vDiffuseUV: vec2f;uniform diffuseMatrix: mat4x4f;uniform vDiffuseInfos: vec2f;
+#endif
+#ifdef POINTSIZE
+uniform pointSize: f32;
+#endif
+varying vPositionW: vec3f;
+#ifdef NORMAL
+varying vNormalW: vec3f;
+#endif
+#include<clipPlaneVertexDeclaration>
+#include<logDepthDeclaration>
+#include<fogVertexDeclaration>
+#include<__decl__lightVxFragment>[0..maxSimultaneousLights]
+#if defined(CLUSTLIGHT_BATCH) && CLUSTLIGHT_BATCH>0
+varying vViewDepth: f32;
+#endif
+#define CUSTOM_VERTEX_DEFINITIONS
+@vertex
+fn main(input : VertexInputs)->FragmentInputs {
+#define CUSTOM_VERTEX_MAIN_BEGIN
+#include<instancesVertex>
+#include<bonesVertex>
+#include<bakedVertexAnimation>
+var worldPos: vec4f=finalWorld* vec4f(vertexInputs.position,1.0);vertexOutputs.position=uniforms.viewProjection*worldPos;vertexOutputs.vPositionW= worldPos.xyz;
+#ifdef NORMAL
+vertexOutputs.vNormalW=normalize(( finalWorld* vec4f(vertexInputs.normal,0.0)).xyz);
+#endif
+#ifndef UV1
+var uv: vec2f= vec2f(0.,0.);
+#else
+var uv: vec2f=vertexInputs.uv;
+#endif
+#ifndef UV2
+var uv2: vec2f= vec2f(0.,0.);
+#else
+var uv2: vec2f=vertexInputs.uv2;
+#endif
+#ifdef DIFFUSE
+if (uniforms.vDiffuseInfos.x==0.)
+{vertexOutputs.vDiffuseUV=(uniforms.diffuseMatrix* vec4f(uv,1.0,0.0)).xy;}
+else
+{vertexOutputs.vDiffuseUV=(uniforms.diffuseMatrix* vec4f(uv2,1.0,0.0)).xy;}
+#endif
+#include<clipPlaneVertex>
+#include<logDepthVertex>
+#include<fogVertex>
+#include<shadowsVertex>[0..maxSimultaneousLights]
+#define CUSTOM_VERTEX_MAIN_END
+}
+`;e.ShadersStoreWGSL[g]||(e.ShadersStoreWGSL[g]=_);var v=[n,d,i,p,t,m,u,l,r,o,h,f,s,a,c];for(let t of v)e.IncludesShadersStoreWGSL[t.name]||(e.IncludesShadersStoreWGSL[t.name]=t.shader);var y={name:g,shader:_};export{y as normalVertexShaderWGSL};
---
+++ assets/shadowOnly.fragment-C7jnUf9n.js
@@ -0,0 +1,35 @@
+import{t as e}from"./shaderStore-D-XQlhUT.js";import{t}from"./sceneUboDeclaration-B96Tfx7b.js";import{$ as n,H as r,J as i,K as a,U as o,V as s,W as c,ct as l,lt as u,q as d,ut as f}from"./index-GDLd02NM.js";var p=`shadowOnlyPixelShader`,m=`#include<sceneUboDeclaration>
+uniform alpha: f32;uniform shadowColor: vec3f;varying vPositionW: vec3f;
+#ifdef NORMAL
+varying vNormalW: vec3f;
+#endif
+#include<helperFunctions>
+#include<lightUboDeclaration>[0..maxSimultaneousLights]
+#include<lightsFragmentFunctions>
+#include<shadowsFragmentFunctions>
+#include<clipPlaneFragmentDeclaration>
+#include<logDepthDeclaration>
+#include<fogFragmentDeclaration>
+#if defined(CLUSTLIGHT_BATCH) && CLUSTLIGHT_BATCH>0
+varying vViewDepth: f32;
+#endif
+#define CUSTOM_FRAGMENT_DEFINITIONS
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+#include<clipPlaneFragment>
+var viewDirectionW: vec3f=normalize(scene.vEyePosition.xyz-fragmentInputs.vPositionW);
+#ifdef NORMAL
+var normalW: vec3f=normalize(fragmentInputs.vNormalW);
+#else
+var normalW: vec3f= vec3f(1.0,1.0,1.0);
+#endif
+var diffuseBase: vec3f= vec3f(0.,0.,0.);var info: lightingInfo;var shadow: f32=1.;var glossiness: f32=0.;var aggShadow: f32=0.;var numLights: f32=0.;
+#include<lightFragment>[0..1]
+var color: vec4f= vec4f(uniforms.shadowColor,(1.0-clamp(shadow,0.,1.))*uniforms.alpha);
+#include<logDepthFragment>
+#include<fogFragment>
+fragmentOutputs.color=color;
+#define CUSTOM_FRAGMENT_MAIN_END
+}
+`;e.ShadersStoreWGSL[p]||(e.ShadersStoreWGSL[p]=m);var h=[t,f,i,d,a,u,n,c,l,o,r,s];for(let t of h)e.IncludesShadersStoreWGSL[t.name]||(e.IncludesShadersStoreWGSL[t.name]=t.shader);var g={name:p,shader:m};export{g as shadowOnlyPixelShaderWGSL};
