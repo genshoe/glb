66f88aad65ef5de95f5db9b632d805b18dbc6cc8
genshoe <genshoeaddon@gmail.com>
2026-08-14 19:32:41 +0700

    Add files via upload

---
+++ assets/normal.vertex-DG5rHl5M.js
@@ -0,0 +1,66 @@
+import{t as e}from"./shaderStore-D-XQlhUT.js";import{B as t,Bt as n,I as r,L as i,Lt as a,Mt as o,N as s,Nt as c,P as l,Pt as u,R as d,Vt as f,dt as p,ft as m,z as h}from"./index-B-04qbd-.js";var g=`normalVertexShader`,_=`precision highp float;attribute vec3 position;
+#ifdef NORMAL
+attribute vec3 normal;
+#endif
+#ifdef UV1
+attribute vec2 uv;
+#endif
+#ifdef UV2
+attribute vec2 uv2;
+#endif
+#ifdef VERTEXCOLOR
+attribute vec4 color;
+#endif
+#include<bonesDeclaration>
+#include<bakedVertexAnimationDeclaration>
+#include<instancesDeclaration>
+uniform mat4 view;uniform mat4 viewProjection;
+#ifdef DIFFUSE
+varying vec2 vDiffuseUV;uniform mat4 diffuseMatrix;uniform vec2 vDiffuseInfos;
+#endif
+#ifdef POINTSIZE
+uniform float pointSize;
+#endif
+varying vec3 vPositionW;
+#ifdef NORMAL
+varying vec3 vNormalW;
+#endif
+#include<clipPlaneVertexDeclaration>
+#include<logDepthDeclaration>
+#include<fogVertexDeclaration>
+#include<__decl__lightFragment>[0..maxSimultaneousLights]
+#if defined(CLUSTLIGHT_BATCH) && CLUSTLIGHT_BATCH>0
+varying float vViewDepth;
+#endif
+#define CUSTOM_VERTEX_DEFINITIONS
+void main(void) {
+#define CUSTOM_VERTEX_MAIN_BEGIN
+#include<instancesVertex>
+#include<bonesVertex>
+#include<bakedVertexAnimation>
+vec4 worldPos=finalWorld*vec4(position,1.0);gl_Position=viewProjection*worldPos;vPositionW=vec3(worldPos);
+#ifdef NORMAL
+vNormalW=normalize(vec3(finalWorld*vec4(normal,0.0)));
+#endif
+#ifndef UV1
+vec2 uv=vec2(0.,0.);
+#endif
+#ifndef UV2
+vec2 uv2=vec2(0.,0.);
+#endif
+#ifdef DIFFUSE
+if (vDiffuseInfos.x==0.)
+{vDiffuseUV=vec2(diffuseMatrix*vec4(uv,1.0,0.0));}
+else
+{vDiffuseUV=vec2(diffuseMatrix*vec4(uv2,1.0,0.0));}
+#endif
+#include<clipPlaneVertex>
+#include<logDepthVertex>
+#include<fogVertex>
+#include<shadowsVertex>[0..maxSimultaneousLights]
+#if defined(POINTSIZE) && !defined(WEBGPU)
+gl_PointSize=pointSize;
+#endif
+#define CUSTOM_VERTEX_MAIN_END
+}
+`;e.ShadersStore[g]||(e.ShadersStore[g]=_);var v=[f,n,a,m,h,t,l,s,u,c,o,p,r,d,i];for(let t of v)e.IncludesShadersStore[t.name]||(e.IncludesShadersStore[t.name]=t.shader);var y={name:g,shader:_};export{y as normalVertexShader};
---
+++ assets/shadowOnly.vertex-B8QMFco7.js
@@ -0,0 +1,43 @@
+import{t as e}from"./shaderStore-D-XQlhUT.js";import{t}from"./sceneUboDeclaration-B96Tfx7b.js";import{$ as n,At as r,Ct as i,Et as a,Q as o,St as s,X as c,Z as l,et as u,i as d,kt as f,ot as p,st as m,tt as h,xt as g}from"./index-B-04qbd-.js";var _=`shadowOnlyVertexShader`,v=`attribute position: vec3f;
+#ifdef NORMAL
+attribute normal: vec3f;
+#endif
+#include<bonesDeclaration>
+#include<bakedVertexAnimationDeclaration>
+#include<instancesDeclaration>
+#include<sceneUboDeclaration>
+#ifdef POINTSIZE
+uniform pointSize: f32;
+#endif
+varying vPositionW: vec3f;
+#ifdef NORMAL
+varying vNormalW: vec3f;
+#endif
+#ifdef VERTEXCOLOR
+varying vColor: vec4f;
+#endif
+#include<clipPlaneVertexDeclaration>
+#include<logDepthDeclaration>
+#include<fogVertexDeclaration>
+#include<__decl__lightVxFragment>[0..maxSimultaneousLights]
+#if defined(CLUSTLIGHT_BATCH) && CLUSTLIGHT_BATCH>0
+varying vViewDepth: f32;
+#endif
+#define CUSTOM_VERTEX_DEFINITIONS
+@vertex
+fn main(input : VertexInputs)->FragmentInputs {
+#define CUSTOM_VERTEX_MAIN_BEGIN
+#include<instancesVertex>
+#include<bonesVertex>
+#include<bakedVertexAnimation>
+var worldPos: vec4f=finalWorld* vec4f(vertexInputs.position,1.0);vertexOutputs.position=scene.viewProjection*worldPos;vertexOutputs.vPositionW= worldPos.xyz;
+#ifdef NORMAL
+vertexOutputs.vNormalW=normalize(( finalWorld* vec4f(vertexInputs.normal,0.0)).xyz);
+#endif
+#include<clipPlaneVertex>
+#include<logDepthVertex>
+#include<fogVertex>
+#include<shadowsVertex>[0..maxSimultaneousLights]
+#define CUSTOM_VERTEX_MAIN_END
+}
+`;e.ShadersStoreWGSL[_]||(e.ShadersStoreWGSL[_]=v);var y=[r,f,a,t,m,n,h,d,u,i,s,g,p,c,o,l];for(let t of y)e.IncludesShadersStoreWGSL[t.name]||(e.IncludesShadersStoreWGSL[t.name]=t.shader);var b={name:_,shader:v};export{b as shadowOnlyVertexShaderWGSL};
